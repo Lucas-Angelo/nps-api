44a7610d37b2a179dafb313f20adc2821c0df35a
lcs2001 <lcs2001_@hotmail.com>
2021-02-24 15:42:08 -0300

    Segundo teste usando not

--- src/__tests__/First.test.ts
+++ src/__tests__/First.test.ts
@@ -3,9 +3,11 @@
 describe("First", () => {
 
     it("Deve ser possível somar 2 números", () => {
-        
         expect(2 + 2).toBe(4);
+    });
 
-    })
+    it("Deve ser possível somar 2 números", () => {
+        expect(2 + 2).not.toBe(5);
+    });
 
 })
