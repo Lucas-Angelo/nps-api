941719410ba862290975fa796b529181ecb1c56b
lcs2001 <lcs2001_@hotmail.com>
2021-02-25 11:16:20 -0300

    Rota e configuracao do controller para salvar dados d

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,64 @@
+/* 
+    Seria o SurveysUserController, 
+    mais o objetivo principal dele é enviar 
+    o email e depois salvar a resposta.
+*/
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class SendMailController {
+
+    // Método que vai executar o envio de e-mail
+    async execute(request: Request, response: Response) {
+
+        // Irá receber o email do usuário e a pesquisa
+        const { email, survey_id } = request.body;
+
+        // Precisa confirmar se o e-mail existe (cadastrado) na plataforma
+        const usersRepository = getCustomRepository(UsersRepository);
+        // Precisa verificar se a pesquisa também existe
+        const surveysRepository = getCustomRepository(SurveysRepository);
+        // Será o utilizado nos métodos
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        // Verificar se o usuário existe
+        const userAlreadyExists = await usersRepository.findOne({email});
+
+        // Se o usuário não existir, dar erro
+        if(!userAlreadyExists) {
+            return response.status(400).json({
+                error: "User does not exists",
+            });
+        }
+
+        // Capturar no findOne() o id do survey_id
+        const surveyAlreadyExists = await surveysRepository.findOne({id: survey_id});
+    
+        // Se a pesquisa não existir, dar erro
+        if(!surveyAlreadyExists) {
+            return response.status(400).json({
+                error: "Survey does not exists",
+            });
+        }
+        
+        // Se passou disso tudo, agora sim. 
+        // Primeiro: salvar as informações na tabela surveyUser
+        const surveyUser = surveysUsersRepository.create({
+            user_id: userAlreadyExists.id,
+            survey_id: surveyAlreadyExists.id
+        })
+        // Esperando salvar o registro na tabela do banco de dados
+        await surveysUsersRepository.save(surveyUser);
+
+        // Segundo: enviar o email
+
+
+        return response.json(surveyUser);
+    }
+    
+}
+
+export { SendMailController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveyController } from './controllers/SurveyController';
+import { SendMailController } from './controllers/SendMailController';
 
 // Arquivo das rotas de requisições HTTP separadas por arquitetura do server.ts
 
@@ -8,6 +9,7 @@ const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveyController();
+const sendMailController = new SendMailController();
 
 /*
     5 métodos de requisição HTTP mais utilizados:
@@ -23,8 +25,10 @@ router.post("/users", userController.create);
 
 // Para o create de surveys do controller
 router.post("/surveys", surveysController.create);
-
 // Para ver todas as pesquisas (surveys)
 router.get("/surveys", surveysController.show);
+
+// Rota cadastro da pesquisa de um usuario
+router.post("/sendMail", sendMailController.execute);
 
 export { router }; // Retornando as rotas preenchidas para o server.ts
