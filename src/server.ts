e177b527b67551fee82dc000871371198aa2973e
lcs2001 <lcs2001_@hotmail.com>
2021-02-27 18:50:57 -0300

    Variavel ambiente da porta do servidor heroku

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ import { app } from "./app";
     Criando o servidor na porta 3333, para iniciar: yarn dev
     Yarn dev configurado no package.json pelo Ts-node-dev
 */
-app.listen(3333, () => console.log("Server is running!"));
+app.listen((process.env.PORT || 3333), () => console.log("Server is running!"));
